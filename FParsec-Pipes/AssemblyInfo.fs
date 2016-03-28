a26c72c8f0cf560c459754dd18c3459e6079ac62
Robert Peele <robert.s.peele@gmail.com>
2016-03-27 22:26:05 -0400

    Add nuspec and assembly info.

--- FParsec-Pipes/AssemblyInfo.fs
+++ FParsec-Pipes/AssemblyInfo.fs
@@ -10,9 +10,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitle("FParsec-Pipes")>]
 [<assembly: AssemblyDescription("A library for building FParsec parsers using pipeline operators.")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyCompany("Robert Peele")>]
 [<assembly: AssemblyProduct("FParsec-Pipes")>]
-[<assembly: AssemblyCopyright("Copyright © Robert Peele 2015")>]
+[<assembly: AssemblyCopyright("Copyright © Robert Peele 2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
 
 do
     ()
