3eb2510f785aefd131f306c9728f45126a40e820
Robert Peele <robert.s.peele@gmail.com>
2016-03-27 20:23:44 -0400

    Delete old/empty doc files.

--- FParsec-Pipes-Doc/Reference.fsx
+++
@@ -1,9 +0,0 @@
-﻿(*** hide ***)
-
-#r @"bin/debug/FParsecCS.dll"
-#r @"bin/debug/FParsec.dll"
-#r @"bin/debug/FParsec-Pipes.dll"
-open System
-open FParsec
-open FParsec.Pipes
-
--- FParsec-Pipes-Doc/build.fsx
+++ FParsec-Pipes-Doc/build.fsx
@@ -5,6 +5,6 @@ open System.IO
 let source = __SOURCE_DIRECTORY__
 let template = Path.Combine(source, "template.html")
 
-for file in ["Intro.fsx"; "Reference.fsx"] do
+for file in ["Intro.fsx"] do
     Literate.ProcessScriptFile
         (Path.Combine(source, file), template, lineNumbers = false)
